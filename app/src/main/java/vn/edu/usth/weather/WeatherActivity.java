02ca5a26f4bcf837fe91b2f5fa4a03e9d9b802b7
MinhCris <minhdq1611@gmail.com>
2024-09-06 13:48:51 +0700

    second

--- app/src/main/java/vn/edu/usth/weather/WeatherActivity.java
+++ app/src/main/java/vn/edu/usth/weather/WeatherActivity.java
@@ -17,7 +17,11 @@ public class WeatherActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
+
         setContentView(R.layout.activity_weather);
+        ForecastFragment firstFragment = new ForecastFragment();
+        getSupportFragmentManager().beginTransaction().add(
+                R.id.main, firstFragment).commit();
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
@@ -28,35 +32,36 @@ public class WeatherActivity extends AppCompatActivity {
     @Override
     protected void onStart() {
         super.onStart();
-        Log.d(TAG, "onStart called");
+        Log.i(TAG, "onStart called");
         // Activity is becoming visible to the user
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        Log.d(TAG, "onResume called");
+        Log.i(TAG, "onResume called");
         // Activity has become visible (it is now "resumed")
     }
 
     @Override
     protected void onPause() {
         super.onPause();
-        Log.d(TAG, "onPause called");
+        Log.i(TAG, "onPause called");
         // Another activity is taking focus (this activity is about to be "paused")
     }
 
     @Override
     protected void onStop() {
         super.onStop();
-        Log.d(TAG, "onStop called");
+        Log.i(TAG, "onStop called");
         // The activity is no longer visible (it is now "stopped")
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        Log.d(TAG, "onDestroy called");
+
         // The activity is about to be destroyed
     }
 }
+
